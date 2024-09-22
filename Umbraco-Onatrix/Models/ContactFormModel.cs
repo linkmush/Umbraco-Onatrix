716f055c17d26f9370e1acf7d13ace466949795f
oskar lindqvist <lindqvist_11@hotmail.com>
2024-09-23 06:35:36 +0700

    forms on home and contact page styled but dropdownmenu not done

---
+++ Umbraco-Onatrix/Models/ContactFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Umbraco_Onatrix.Models;
+
+public class ContactFormModel
+{
+	public string Name { get; set; } = null!;
+	public string Email { get; set; } = null!;
+	public string Phone { get; set; } = null!;
+}
